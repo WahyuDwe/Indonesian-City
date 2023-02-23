5056d878646837d55f64349c04be3e2bd128ff01
dwi <wdwihartarto@gmail.com>
2023-02-23 15:07:11 +0700

    add new fonts

--- app/src/main/java/com/example/indonesiancity/activity/SplashActivity.kt
+++ app/src/main/java/com/example/indonesiancity/activity/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.example.indonesiancity.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.example.indonesiancity.R
 
 
